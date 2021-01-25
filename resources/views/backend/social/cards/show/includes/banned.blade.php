0fa2fbeb6c90a80cd7d772dbc399742b60b8995f
Moods YKLM <moodsyklm@gmail.com>
2021-01-26 04:43:01 +0800

    寫相反了

--- resources/views/backend/social/cards/show/includes/banned.blade.php
+++ resources/views/backend/social/cards/show/includes/banned.blade.php
@@ -1,4 +1,4 @@
-@if (! $media->isBanned())
+@if ($media->isBanned())
 <span class="badge badge-success p-1" data-toggle="tooltip" data-placement="bottom"
     title="{{ isset($media->banned_remarks)? $media->banned_remarks : '尚未填寫原因。' }}">@lang('labels.general.yes')</span>
 @else
